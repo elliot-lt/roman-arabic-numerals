2293c6073976848f41070edb2a67927f6591e430
Elliot Iddon <elliot@land.tech>
2023-03-10 11:57:06 +0000

    Roman to Arabic and back again

--- convert.py
+++ convert.py
@@ -1,16 +1,28 @@
 # https://en.wikipedia.org/wiki/Roman_numerals
 
 LOOKUP = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+REV_LOOKUP = {1: "I", 4: "IV", 5: "V", 9: "IX", 10: "X", 40: "XL", 50: "L", 90: "XC", 100: "C", 400: "CD", 500: "D", 900: "CM", 1000: "M"}
 
 def roman_to_arabic(roman: str) -> int:
     total = 0
     last_char = 0
-    for char in roman:
+    for char in reversed(roman):
         this_char = LOOKUP[char]
-        if this_char > last_char:
-            total = total + this_char-(last_char*2)
+        if this_char < last_char:
+            total -= this_char
         else:
             total += this_char
         last_char = this_char
 
     return total
+
+
+def arabic_to_roman(arabic: int) -> str:
+    result = ""
+    while arabic:
+        reduce_by = max(k for k in REV_LOOKUP if k <= arabic)
+        result += REV_LOOKUP[reduce_by]
+        arabic -= reduce_by
+
+    return result
+
--- test_convert.py
+++ test_convert.py
@@ -1,5 +1,6 @@
 import pytest
 import convert
+import itertools
 
 # https://en.wikipedia.org/wiki/Roman_numerals
 @pytest.mark.parametrize("roman,arabic", (
@@ -20,3 +21,35 @@ import convert
 ))
 def test_roman_to_arabic(roman: str, arabic: int):
     assert convert.roman_to_arabic(roman) == arabic
+
+@pytest.mark.parametrize("arabic,roman", (
+    (1, "I"),
+    (2, "II"),
+    (3, "III"),
+    (5, "V"),
+    (6, "VI"),
+    (4, "IV"),
+    (10, "X"),
+    (50, "L"),
+    (100, "C"),
+    (500, "D"),
+    (1000, "M"),
+    (160, "CLX"),
+    (140, "CXL"),
+    (39, "XXXIX"),
+    (2421, "MMCDXXI"),
+    (3999, "MMMCMXCIX"),
+))
+def test_arabic_to_roman(arabic, roman):
+    assert convert.arabic_to_roman(arabic) == roman
+
+def test_no_quartets():
+    for n in range(1, 4000):
+        roman = convert.arabic_to_roman(n)
+        # print(roman)
+        for _, block in itertools.groupby(roman):
+            assert len(list(block)) < 4
+
+def test_inverts():
+    for n in range(1, 4000):
+        assert convert.roman_to_arabic(convert.arabic_to_roman(n)) == n
